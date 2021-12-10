e16e585a4823e80fa6f95071bfa6c79af16bf6fc
Szymon Datko <szymon.datko@pwr.edu.pl>
2021-12-11 00:47:51 +0100

    Add support for checking certificates in CLI
    This commit introduces the basic functionality of checking
    SSL certificates of hosts and files with the CLI interface.
    It utilizes the previously added functions.

--- certchecker/cli.py
+++ certchecker/cli.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
+
+from certchecker.expiration import get_cert_expiration_from_file
+from certchecker.expiration import get_cert_expiration_from_host
+from certchecker.expiration import get_days_to_expiration
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str, default=None,
+                        help='path to local file to verify')
+    parser.add_argument('-t', '--host', type=str, default=None,
+                        help='host with certificate to verify')
+    parser.add_argument('-p', '--port', type=int, default=443,
+                        help='port on host with certificate, default 443')
     args = parser.parse_args()
+
+    if not (args.file or args.host):
+        parser.error('At least one of --file or --host is required.')
+
+    if args.file is not None:
+        date = get_cert_expiration_from_file(args.file)
+    if args.host is not None:
+        date = get_cert_expiration_from_host(args.host, args.port)
+
+    days = get_days_to_expiration(date)
+
+    print(days)
+
+    if days < 0:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
