57b6780017571255417d323bfe6a3a208360082e
Szymon Datko <szymon.datko@pwr.edu.pl>
2021-12-13 20:36:17 +0100

    Add PoC of authentication with second factor
    This commit contains a prototype of authentication
    with second factor mechanism utilizing the TOTP tokens.
    It is not intended as a production solution in current
    version and shall be only used for demonstration purposes.

--- certchecker/web.py
+++ certchecker/web.py
@@ -1,21 +1,53 @@
 #!/usr/bin/env python3
 
+from random import random
 import threading
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+import pyotp
 
 
 server_results = {}
 
 
 def view():
-    return render_template('index.html', results=server_results)
+    if request.form:
+        if request.form.get('user') and request.form.get('pass'):
+            if (request.form.get('user') == 'admin'
+                    and request.form.get('pass') == 'qwerty'):
+                session['secondfactor'] = True
+            else:
+                session['authenticated'] = False
+                session['secondfactor'] = False
+        elif request.form.get('token') and session.get('secondfactor', False):
+            totp = pyotp.TOTP('base32secret3232')
+            if totp.verify(request.form.get('token'), valid_window=3):
+                session['authenticated'] = True
+            else:
+                session['authenticated'] = False
+                session['secondfactor'] = False
+        else:
+            session['authenticated'] = False
+            session['secondfactor'] = False
+        return redirect(url_for('index'))
+
+    authenticated = session.get('authenticated', False)
+    secondfactor = session.get('secondfactor', False)
+    return render_template('index.html',
+                           authenticated=authenticated,
+                           secondfactor=secondfactor,
+                           results=server_results)
 
 
 def main():
     app = Flask(__name__)
-    app.add_url_rule('/', 'index', view_func=view)
+    app.add_url_rule('/', 'index', view_func=view, methods=['GET', 'POST'])
+    app.secret_key = 'development key ' + str(random())
     app.run(port=5000, debug=False, use_reloader=False)
 
 
