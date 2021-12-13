f486f4ba2a650b4469374aaab5c721a7364bf021
Szymon Datko <szymon.datko@pwr.edu.pl>
2021-12-13 18:11:35 +0100

    Add module init file

---
+++ certchecker/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+CertChecker – Application to remind about SSL certificates renewal
+"""
+
+__all__ = [
+    "__author__", "__copyright__", "__email__", "__license__",
+    "__summary__", "__title__", "__uri__", "__version__",
+]
+
+__version__ = "1.0.0"
+__title__ = "CertChecker"
+__uri__ = "https://github.com/sdatko/certchecker/"
+__summary__ = "Application to remind about SSL certificates renewal"
+__author__ = "Szymon Datko"
+__email__ = "szymon.datko@gmail.com"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021 {0}".format(__author__)
